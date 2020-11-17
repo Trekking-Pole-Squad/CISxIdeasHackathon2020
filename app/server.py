1930681a2ef1203042442a45cb7737570347539c
Ryan Wong <ryan.wong@kais-air-2.mynet>
2020-11-17 21:49:39 +0800

    binretriever class, pickle file + debugging

--- app/server.py
+++ app/server.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 from fastapi import FastAPI
-from bins.binretriever import BinRetriever
+from binretriever import *
 import asyncio
 
 app = FastAPI()
-bin_retriever = BinRetriever()
+binretriever = BinRetriever()
 
 @app.get("/bincoords/")
-async def output_bin_coords(lat: int = 91, lon: int = 181, rng_lat: int = 0.001):
+async def output_bin_coords(lat: int = 91, lon: int = 181, rng_lat: int = 0.001, returnall: bool = False):
 	return_value = False
-	if abs(lat) <= 90 or abs(lon) <= 180:
+	if returnall:
+		return_value = binretriever.bins
+	elif abs(lat) <= 90 or abs(lon) <= 180:
 		return_value = binretriever.near(lat, lon, rng_lat)
 	return return_value
 
