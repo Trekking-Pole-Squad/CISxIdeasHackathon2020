05f3ec21f7c6262f6cae62b5db36c518cf2f4bae
Ryan Wong <ryan.wong@kais-air-2.mynet>
2020-11-17 21:28:42 +0800

    updated binretriever

--- app/server.py
+++ app/server.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
 from fastapi import FastAPI
-from bins.get_bin import Get_Bin
+from bins.binretriever import BinRetriever
 import asyncio
 
 app = FastAPI()
-get_bin = Get_Bin()
+bin_retriever = BinRetriever()
 
 @app.get("/bincoords/")
 async def output_bin_coords(lat: int = 91, lon: int = 181, rng_lat: int = 0.001):
 	return_value = False
 	if abs(lat) <= 90 or abs(lon) <= 180:
-		return_value = get_bin.near(lat, lon, rng_lat)
+		return_value = binretriever.near(lat, lon, rng_lat)
 	return return_value
 
 
