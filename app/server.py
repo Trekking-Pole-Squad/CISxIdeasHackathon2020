fd24fde8819eca09ec722af3ccf4f0ed84886af6
yu205619 <58062663+yu205619@users.noreply.github.com>
2020-11-17 15:50:42 +0800

    Create simple webpage

--- app/server.py
+++ app/server.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/webclient",StaticFiles(directory="webclient"))
 
 if __name__ == "__main__":
 	import uvicorn
