9b805f0ec29f938840969381820d3cc6d25c559a
yu205619 <58062663+yu205619@users.noreply.github.com>
2020-11-17 16:13:21 +0800

    Add location tracking system

--- app/webclient/index.js
+++ app/webclient/index.js
@@ -5,11 +5,40 @@ var view = new ol.View({
 	zoom: 0
 });
 
+var geolocation = new ol.Geolocation({
+	projection: view.getProjection(),
+	tracking: true
+});
+var positionFeature = new ol.Feature();
+positionFeature.setStyle(
+	new ol.style.Style({
+		image: new ol.style.Circle({
+			radius: 8,
+			fill: new ol.style.Fill({
+				color: "#3399CC"
+			}),
+			stroke: new ol.style.Stroke({
+				color: "#FFFFFF",
+				width: 2
+			})
+		})
+	})
+);
+geolocation.on("change:position", function() {
+	let coords = geolocation.getPosition();
+	positionFeature.setGeometry(coords ? new ol.geom.Point(coords) : null);
+});
+
 var map = new ol.Map({
 	target: "map",
 	layers: [
 		new ol.layer.Tile({
 			source: new ol.source.OSM()
+		}),
+		new ol.layer.Vector({
+			source: new ol.source.Vector({
+				features: [positionFeature]
+			})
 		})
 	],
 	view: view
