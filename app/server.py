f3bdfaffdedc36e86a1c18635eb8a1e32d340ae6
Ryan Wong <ryan.wong@kais-air-2.mynet>
2020-11-18 22:40:33 +0800

    removed id system and added token system usernames now have to be unique added new setup.py file

--- app/server.py
+++ app/server.py
@@ -9,22 +9,33 @@ app = FastAPI()
 binretriever = BinRetriever()
 users = Users()
 
+@app.get("/gettoken/")
+async def output_token(username: str = "", sha512_password: str = ""):
+    return_value = False
+    if users.auth_password(username, sha512_password):
+        users.create_token(username)
+        return_value = {"token":users.token_from_user(username)}
+    return return_value
+
 @app.get("/bincoords/")
 async def output_bin_coords(lat: float = 91, lon: float = 181, rng_lat: float = 0.001, returnall: bool = False):
-	return_value = False
-	if returnall:
-		return_value = binretriever.bins
-	elif abs(lat) <= 90 or abs(lon) <= 180:
-		return_value = binretriever.near(lat, lon, rng_lat)
-	return return_value
+    return_value = False
+    if returnall:
+        return_value = binretriever.bins
+    elif abs(lat) <= 90 or abs(lon) <= 180:
+        return_value = binretriever.near(lat, lon, rng_lat)
+    return return_value
 
 @app.get("/inventory/")
-async def output_user_inventory(id: int = 0):
-	return_value = False
-	return_value = users.get_inventory(id)
-	return return_value
-	
+async def output_user_inventory(token: str = ""):
+    return_value = False
+    if users.auth_token(token):
+        return_value = users.get_inventory(username)
+    else:
+        return_value = {"detail":"Not authenticated"}
+    return return_value
+
 
 if __name__ == "__main__":
-	import uvicorn
-	uvicorn.run(app)
+    import uvicorn
+    uvicorn.run(app)
