d05c57f6aa2cd8759f518b5a416bb3b1301f8be1
yu205619 <58062663+yu205619@users.noreply.github.com>
2020-11-21 22:11:50 +0800

    Remove redundant "GET /bincoords/" feature

--- app/server.py
+++ app/server.py
@@ -2,12 +2,10 @@
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from binretriever import *
 from users import *
 import asyncio
 
 app = FastAPI()
-binretriever = BinRetriever()
 users = Users()
 
 @app.get("/gettoken/")
@@ -16,15 +14,6 @@ async def output_token(username: str = "", sha512_password: str = ""):
     if users.auth_password(username, sha512_password):
         users.create_token(username)
         return_value = {"token":users.token_from_user(username)}
-    return return_value
-
-@app.get("/bincoords/")
-async def output_bin_coords(lat: float = 91, lon: float = 181, rng_lat: float = 0.001, returnall: bool = False):
-    return_value = False
-    if returnall:
-        return_value = binretriever.bins
-    elif abs(lat) <= 90 or abs(lon) <= 180:
-        return_value = binretriever.near(lat, lon, rng_lat)
     return return_value
 
 @app.get("/inventory/")
